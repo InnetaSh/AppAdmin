d72e0a7e5716a6f0b1c84697c593af71c082e107
InnetaSh <inneta3@gmail.com>
2025-03-24 08:53:47 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using AppAdmin.Models;
+
+namespace AppAdmin
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        internal static List<Admin> Admins = new List<Admin>();
+    }
+
+}
